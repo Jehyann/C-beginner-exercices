eaf1bbaf5b71e2f4b164a501e57222280675753c
Jehyann <115532715+Jehyann@users.noreply.github.com>
2023-03-21 16:20:24 +0100

    Add files via upload

--- ex5.c
+++ ex5.c
@@ -3,3 +3,30 @@
 // ces deux nombres sont égaux ou non. S'ils sont différents, dite qui est supérieur ou inférieur et
 // l’écart entre les deux nombres.
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n1;
+    int n2;
+
+    printf("\n\n Enter your first number : ");
+    scanf("%d",&n1);
+
+    printf("\n\n Enter the second number : ");
+    scanf("%d",&n2);
+
+    if (n1 < n2) {
+        int res = n2 - n1;
+        printf("\t\tThe second number (%d) is bigger, there is a difference of %d.\n",n2,res);
+    }
+    else if (n1 > n2){
+         int res = n1 - n2;
+        printf("\t\tThe first number (%d) is bigger, there is a difference of %d.\n",n1,res);
+    }
+    else{
+        printf("\t\tThey are the same.\n");
+    }
+    return 0;
+}
--- ex6.c
+++ ex6.c
@@ -2,3 +2,52 @@
 // Dans cet exercice, nous allons demander à l’utilisateur de rentrer un nombre entier entre 1 et 7.
 // Chaque nombre entier correspondra à un jour de la semaine. Gérer aussi le cas où l’utilisateur rentre
 // un nombre invalide.
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+    int number;
+
+    printf("\n\n Enter a number between 1 and 7 : ");
+    scanf("%d\n",&number);
+
+    switch (number)
+    {
+        case 1:
+            printf("Monday");
+        break;
+
+        case 2:
+            printf("Tuesday");
+        break;
+
+        case 3:
+            printf("Wednesday");
+        break;
+
+        case 4:
+            printf("Thursday");
+        break;
+
+        case 5:
+            printf("Friday");
+        break;
+
+        case 6:
+            printf("Saturday");
+        break;
+
+        case 7:
+            printf("Sunday");
+        break;
+            
+        default:
+            printf("Wrong number!");
+        break;
+        }
+
+    return 0;
+
+}
