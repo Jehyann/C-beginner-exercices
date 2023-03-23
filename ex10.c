8f1aacd536ba207fa88926e020d6829047005b9e
Jehyann <115532715+Jehyann@users.noreply.github.com>
2023-03-23 17:45:15 +0100

    Add files via upload

--- ex10.c
+++ ex10.c
@@ -3,3 +3,25 @@
 // nombre est premier. (Pour rappel : un nombre premier est un nombre qui n’est divisible que par 1
 // et lui-même.).
 
+#include <stdio.h>
+
+
+int main (){
+    
+    int n, i;
+    printf("\nPlease enter your choosen number (more than 1) : ");
+    scanf("%d", &n);
+
+    if (n == 0 || n == 1) {
+        printf("Please enter a correct number.");
+    };
+
+    for (i = 2; i <= n / 2; ++i) {
+        if (n % i == 0)
+            printf("This is not a prime number.");
+        else
+            printf("This is a prime number.");
+    };
+
+    return 0;
+}
--- ex8.c
+++ ex8.c
@@ -3,3 +3,22 @@
 // cosinus de zéro jusqu’à ce nombre par pas de 1. Par exemple si l’utilisateur tape 3, le programme
 // affichera le cos (0), cos (1), cos (2), cos (3). Évidemment, le nombre sera compris entre 0 et 360.
 
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int n, i;
+    float res;
+
+    printf("\nGive a number between 0 and 360 to calculate the cosinus :");
+    scanf("%d", &n);
+
+    for (int i = 0; i <= n; i++){
+        // attribut la valeur a res
+        double res = cos(i);
+        printf("\ncos(%d) = %f", i, res);
+    };
+
+    return 0;
+}
